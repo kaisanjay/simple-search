47691983e0eefebdc4a14f9e576ee780cad3fd57
Sanjay sharma <sanjaykai.1994@gmail.com>
2018-08-20 12:29:19 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import { tutorials as tutorialData } from '../data'
+
+export default {
+  name: 'App',
+  data: () => ({
+    tutorials: []
+  }),
+  methods: {
+    filterTutorials: function() {
+        this.tutorials = tutorialData
+    }
+  },
+  created: function() {
+    this.filterTutorials()
+  }
+
+}
+</script>
+
+<style scoped>
+  .container {
+    max-width: 750px;
+  }
+  .footer {
+    padding: 15px;
+    color: #777;
+    border-top: 1px solid #e5e5e5;
+  }
+  .header {
+    margin: 10px 0 20px;
+  }
+</style>
