c58133c207574a9b7bba41831b30d96752b4fe11
Sanjay sharma <sanjaykai.1994@gmail.com>
2018-08-20 12:35:10 +0530

    add TutorialList

---
+++ src/components/TutorialList.vue
@@ -0,0 +1,30 @@
+<script>
+import Tutorial from "./Tutorial";
+
+export default {
+  name: "tutorial-list",
+  components: { Tutorial },
+  props: ["tutorials"]
+};
+</script>
+
+
+
+<style scoped>
+.tutorial-list {
+  margin: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  flex-flow: wrap;
+}
+
+.tutorial {
+  padding: 20px;
+}
+
+@media only screen and (min-width: 600px) {
+  .tutorial {
+    width: 50%;
+  }
+}
+</style>
